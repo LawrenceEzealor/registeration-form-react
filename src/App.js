62d501fd1365fb643408798d13b858c60956b3ad
LawrenceEzealor <ezealor20@gmail.com>
2023-09-11 19:33:35 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Login } from "./Login"
+import { Register } from "./Register"
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (forName) => {
+    setCurrentForm(forName);
+  }
   return (
     <div className="App">
-      <h1>REGISTERATION FORM </h1>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+      }
     </div>
   );
 }
